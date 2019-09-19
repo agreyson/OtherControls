9569496e0923386e3cb623a8d2b991f35c8c6953
agreyson <amgreyson@gmail.com>
2019-09-19 12:55:20 -0700

    Added stepper.

--- OtherControls/ViewController.swift
+++ OtherControls/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func segmentedController(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            label.text = "First segment"
+        } else if sender.selectedSegmentIndex == 1 {
+            label.text = "Second segment"
+        }
+    }
+    
+    @IBAction func slider(_ sender: UISlider) {
+        label.text = "Slider is at \(sender.value)"
+    }
+    
+    @IBAction func `switch`(_ sender: UISwitch) {
+//        if sender.isOn {
+//            label.text = "Switch is on"
+//        } else {
+//            label.text = "Switch is off"
+//        }
+        
+        let theText = "Switch is " + (sender.isOn ? "on" : "off")
+        label.text = theText
+    }
+    
+    @IBAction func stepper(_ sender: UIStepper) {
+        label.text = "Stepper value is \(Int(sender.value))"
+    }
 }
 
